1aa92f9eaeebdd424ef11b99d291d79c1b55f977
Kyan Slack <157538557+KS2A@users.noreply.github.com>
2024-09-30 11:53:13 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "pixel.h"
 
+void average_colors(std::vector<Pixel>& pixel_list) {
+
+}
+
+void flip_vertically(std::vector<Pixel>& pixel_list) {
+
+}
+
+int main() {
+	std::vector<Pixel> pixel_list;
+
+
+	return 0;
+}
